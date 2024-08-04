ac3a6d0002aeb2b419a81e957ec0b687002853fd
Mobin Valikhani <103781147+M0BIN-V@users.noreply.github.com>
2024-08-04 20:54:56 +0330

    feat: add db context

---
+++ Src/Infrastructure/Persistence/ReadDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Persistence;
+
+internal class ReadDbContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; }
+
+    public ReadDbContext() { }
+
+    public ReadDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseLazyLoadingProxies();
+
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
