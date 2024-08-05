92a4cde2ab54e83fad6c2802c50fb30cf6af1bb0
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-08-05 05:16:13 +0330

    feat: create dependencyInjection setup for persistence layer

---
+++ Src/Infrastructure/Persistence/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Interfaces.Persistence.Repositories.Read;
+using Domain.Interfaces.Persistence.Repositories.Write;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.DbContexts;
+using Persistence.Repositories.Read;
+using Persistence.Repositories.Write;
+
+namespace Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddReadRepositories
+        (this IServiceCollection services, Action<DbContextOptionsBuilder>? optionsAction = null)
+    {
+        services.AddDbContext<ReadDbContext>(optionsAction);
+
+        services.AddSingleton<IReadCustomersRepository, ReadCustomersRepository>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddWriteRepositories
+        (this IServiceCollection services, Action<DbContextOptionsBuilder>? optionsAction = null)
+    {
+        services.AddDbContext<WriteDbContext>(optionsAction);
+
+        services.AddSingleton<IWriteCustomersRepository, WriteCustomerRepository>();
+
+        return services;
+    }
+}
