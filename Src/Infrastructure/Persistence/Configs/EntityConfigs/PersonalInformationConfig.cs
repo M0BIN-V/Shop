3bd0cd6c90c8645a1526ad5bd7c106c8d9d81c4c
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-08-05 05:13:42 +0330

    feat: add configuration for personal informations

---
+++ Src/Infrastructure/Persistence/Configs/EntityConfigs/PersonalInformationConfig.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configs.EntityConfigs.EntityConfigurations;
+
+internal class PersonalInformationConfig : IEntityTypeConfiguration<PersonalInformation>
+{
+    public void Configure(EntityTypeBuilder<PersonalInformation> builder)
+    {
+        builder.HasIndex(c => c.PhoneNumber);
+
+        #region Convevtions
+        builder.Property(p => p.FirstName)
+            .HasConversion(n => n!.Value, v => new() { Value = v });
+
+        builder.Property(p => p.LastName)
+            .HasConversion(l => l!.Value, v => new() { Value = v });
+
+        builder.Property(p => p.PhoneNumber)
+            .HasConversion(p => p.Value, v => new() { Value = v });
+        #endregion
+    }
+}
