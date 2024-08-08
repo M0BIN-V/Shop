d36308911faedeec143964554b7ebe643dfaaef6
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-08-08 22:03:22 +0330

    reafactor: move service config to DependencyInjection class

---
+++ Src/Peresentation/Api/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿namespace Api;
+
+public static class DependencyInjection
+{
+    public static WebApplicationBuilder AddServices(this WebApplicationBuilder builder)
+    {
+        builder.Services
+
+            .AddEndpointsApiExplorer();
+
+        return builder;
+    }
+}
---
+++ Src/Peresentation/Api/PipeLine.cs
@@ -0,0 +1,11 @@
+﻿namespace Api;
+
+public static class PipeLine
+{
+    public static WebApplication ConfigurePipeLine(this WebApplication app)
+    {
+        app.UseHttpsRedirection();
+
+        return app;
+    }
+}
