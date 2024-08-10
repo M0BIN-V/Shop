0fcdf4521469d69da9000fe3ca55cdc9978133ef
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-08-10 22:57:28 +0330

    feat: add application and persistence to services

--- Src/Peresentation/Api/Config/DependencyInjection.cs
+++ Src/Peresentation/Api/Config/DependencyInjection.cs
@@ -6,9 +6,25 @@ public static class DependencyInjection
     {
         builder.Services
 
+            .AddApplicationHandlers()
+
+            .AddPersistence(builder.Configuration)
+
             .AddEndpointsApiExplorer()
             .AddGraphQLServer();
 
         return builder;
+    }
+
+    static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration config)
+    {
+        var connectionString = config.GetConnectionString("SqlServer");
+
+        void Action(DbContextOptionsBuilder o) => o.UseSqlServer(connectionString);
+
+        services.AddReadRepositories(Action);
+        services.AddWriteRepositories(Action);
+
+        return services;
     }
 }
