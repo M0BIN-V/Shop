083d02b52f34b42b438c43bcf4841abbd8e003e7
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-08-12 16:18:07 +0330

    feat: add PhoneNumber scalar type

---
+++ Src/Peresentation/Api/Schema/Types/Scalars/PhoneNumberScalarType.cs
@@ -0,0 +1,23 @@
+﻿using HotChocolate.Language;
+
+namespace Api.Schema.Types.Scalars;
+
+public class PhoneNumberScalarType : ScalarType<PhoneNumber, StringValueNode>
+{
+    public PhoneNumberScalarType(BindingBehavior bind = BindingBehavior.Explicit) : base(nameof(PhoneNumber), bind) { }
+
+    public override IValueNode ParseResult(object? resultValue)
+    {
+        return ParseValue(resultValue);
+    }
+
+    protected override PhoneNumber ParseLiteral(StringValueNode valueSyntax)
+    {
+        return new PhoneNumber { Value = valueSyntax.Value };
+    }
+
+    protected override StringValueNode ParseValue(PhoneNumber runtimeValue)
+    {
+        return new StringValueNode(runtimeValue.Value);
+    }
+}
