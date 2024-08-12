ff7bf82a99529a3d0a1980153283095948f8d135
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-08-12 05:17:53 +0330

    fix: fix RepositoryBase

--- Src/Infrastructure/Persistence/Repositories/Abstractions/RepositoryBase.cs
+++ Src/Infrastructure/Persistence/Repositories/Abstractions/RepositoryBase.cs
@@ -1,16 +1,16 @@
 ﻿using Domain.Entities.Abstractions;
 using Microsoft.EntityFrameworkCore;
-using Persistence.DbContexts;
+using Persistence.DbContexts.Common;
 
 namespace Persistence.Repositories.Abstractions;
 
 public abstract class RepositoryBase<TEntity>
 where TEntity : EntityBase
 {
-    protected readonly ReadDbContext _context;
+    protected readonly DbContextBase _context;
     protected readonly DbSet<TEntity> _set;
 
-    protected RepositoryBase(ReadDbContext context)
+    protected RepositoryBase(DbContextBase context)
     {
         _context = context;
         _set = context.Set<TEntity>();
