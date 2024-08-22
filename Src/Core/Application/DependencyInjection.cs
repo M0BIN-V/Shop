74a554ff6cc5aecfb3e05befad894843ff0d74dd
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-08-22 19:29:21 +0330

    fix: add ObjectValidator service

--- Src/Core/Application/DependencyInjection.cs
+++ Src/Core/Application/DependencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Behaviors;
 using Microsoft.Extensions.DependencyInjection;
+using Validity.DataAnnotation;
 
 namespace Application;
 
@@ -7,6 +8,8 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationHandlers(this IServiceCollection services)
     {
+        services.AddObjectValidator();
+
         services.AddMediatR(config =>
         {
             config
