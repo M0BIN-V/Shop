149bcb62304b06fdbd3aefac67b4e8f1df554e85
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-08-23 21:46:17 +0330

    refactor: add global usings in peresentation

--- Src/Peresentation/Api/GlobalUsings.cs
+++ Src/Peresentation/Api/GlobalUsings.cs
@@ -1,1 +1,5 @@
 ﻿global using Domain.ValueObjects;
+global using MediatR;
+global using Microsoft.AspNetCore.Mvc;
+global using Api.Abstractions;
+global using static Microsoft.AspNetCore.Http.StatusCodes;
