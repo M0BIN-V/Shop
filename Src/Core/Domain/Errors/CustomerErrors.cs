76607c55a8827cec3729e4c02938cf02dcc8f00b
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-08-26 14:18:37 +0330

    refactor: rename CustomerNotFoundError

--- Src/Core/Domain/Errors/CustomerErrors.cs
+++ Src/Core/Domain/Errors/CustomerErrors.cs
@@ -2,5 +2,5 @@
 
 namespace Domain.Errors;
 
-public class CustomerNotFountError() : ResultError("Customer not found");
+public class CustomerNotFoundError() : ResultError("Customer not found");
 public class CustomerAlreadyExistsError() : ResultError("Customer already exists");
