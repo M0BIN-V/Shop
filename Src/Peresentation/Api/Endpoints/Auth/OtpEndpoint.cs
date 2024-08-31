05b9284e366d9fa834ba0cec847503b7f9d51c61
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-08-31 20:08:05 +0330

    feat: add otp endpoint

---
+++ Src/Peresentation/Api/Endpoints/Auth/OtpEndpoint.cs
@@ -0,0 +1,21 @@
+﻿using Api.Abstractions.Endpoints;
+using Application.Commands.Auth;
+
+namespace Api.Endpoints.Auth;
+
+public class OtpEndpoint : ResultBaseEndpoint
+{
+    public override void ConfigureEndpoint(IEndpointRouteBuilder builder)
+    {
+        builder.MapPost("auth/send-otp", async ([FromBody] string phoneNumber, IMediator mediator) =>
+        {
+            var phoneNumberValueObject = new PhoneNumber { Value = phoneNumber };
+
+            var command = new SendOtpRequest(phoneNumberValueObject);
+
+            var result = await mediator.Send(command);
+
+            return FromResult(result, () => Results.Ok());
+        });
+    }
+}
