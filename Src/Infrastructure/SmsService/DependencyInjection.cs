c1396f1a790e6d4fa9d0407168423a860e451816
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-08-31 19:43:14 +0330

    feat: add SmsService

---
+++ Src/Infrastructure/SmsService/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Application.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddSmsService
+        (this IServiceCollection services)
+    {
+        services.AddSingleton<ISmsService, SmsService.SmsServiceProvider>();
+
+        return services;
+    }
+}
---
+++ Src/Infrastructure/SmsService/SmsServiceProvider.cs
@@ -0,0 +1,15 @@
+﻿using Application.Interfaces;
+using Domain.ValueObjects;
+
+namespace SmsService;
+
+public class SmsServiceProvider : ISmsService
+{
+    public Task SendOtpAsync(PhoneNumber phoneNumber, string otp)
+    {
+        //TODO Connect to sms panel
+
+        Console.WriteLine($"{phoneNumber.Value} OTP : {otp}");
+        return Task.CompletedTask;
+    }
+}
