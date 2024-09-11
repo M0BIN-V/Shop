51cf9917d2b3de073da5e21703cf992f7ded4f08
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-09-11 13:17:56 +0330

    chore: add error profiles

---
+++ Src/Peresentation/Api/ErrorProfiles/CustomerErrors.cs
@@ -0,0 +1,15 @@
+﻿using Api.Services.ResultErrorHandler.Abstraction;
+
+namespace Api.ErrorProfiles;
+
+public class CustomerErrors : ErrorProfile
+{
+    public override void ConfigureErrors()
+    {
+        AddError<CustomerAlreadyExistsError>()
+            .WithStatusCode(Status409Conflict);
+
+        AddError<CustomerNotFoundError>()
+            .WithStatusCode(Status404NotFound);
+    }
+}
---
+++ Src/Peresentation/Api/ErrorProfiles/ValidationErrorProfile.cs
@@ -0,0 +1,26 @@
+﻿using Api.Services.ResultErrorHandler.Abstraction;
+
+namespace Api.ErrorProfiles;
+
+public class ValidationErrorProfile : ErrorProfile
+{
+    public override void ConfigureErrors()
+    {
+        AddError<ValidationError>()
+            .HandleWith(error =>
+            {
+                var splitErrorTitle = error.Title!.Split('.');
+
+                var fieldName = splitErrorTitle[^1] is "Value" ?
+                    splitErrorTitle[^2] : splitErrorTitle[^1];
+
+                var errors = new Dictionary<string, string[]>
+                {
+                    { fieldName.ToLower(), [error.Message] }
+                };
+
+                return Results.ValidationProblem(errors);
+            })
+            .WithStatusCode(Status400BadRequest);
+    }
+}
