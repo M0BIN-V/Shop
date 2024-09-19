d5589c365da742b9e4b0ac4e594dfadd21c7f3f6
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-09-19 18:50:22 +0330

    fix: fix and merge customer registration with login

--- Src/Core/Application/Commands/Auth/LoginWithOtpHandler.cs
+++ Src/Core/Application/Commands/Auth/LoginWithOtpHandler.cs
@@ -8,22 +8,43 @@ public record LoginWithOtpCommand(PhoneNumber PhoneNumber, string Otp) : IReques
 
 public class LoginWithOtpHandler : IRequestHandler<LoginWithOtpCommand, Result<string>>
 {
+    readonly IReadCustomersRepository _readCustomersRepository;
+    readonly IWriteCustomersRepository _writeCustomersRepository;
     readonly IOtpService _otpService;
     readonly IJwtService _jwtService;
 
-    public LoginWithOtpHandler(IOtpService otpService, IJwtService jwtService)
+    public LoginWithOtpHandler(
+        IOtpService otpService,
+        IJwtService jwtService,
+        IReadCustomersRepository readCustomersRepository,
+        IWriteCustomersRepository writeCustomersRepository)
     {
         _otpService = otpService;
         _jwtService = jwtService;
+        _readCustomersRepository = readCustomersRepository;
+        _writeCustomersRepository = writeCustomersRepository;
     }
 
-    public Task<Result<string>> Handle(LoginWithOtpCommand request, CancellationToken cancellationToken)
+    public async Task<Result<string>> Handle(LoginWithOtpCommand request, CancellationToken cancellationToken)
     {
         var otp = _otpService.GetByKey(request.PhoneNumber.Value);
 
         if (otp is null || otp != request.Otp)
         {
             return new Result<string>(new OtpIsNotValidError());
+        }
+
+        if (!await _readCustomersRepository.ExistsAsync(request.PhoneNumber))
+        {
+            var newCustomer = new Customer
+            {
+                PersonalInformation = new()
+                {
+                    PhoneNumber = request.PhoneNumber
+                }
+            };
+
+            await _writeCustomersRepository.AddAsync(newCustomer);
         }
 
         _otpService.Deprecate(request.PhoneNumber.Value);
