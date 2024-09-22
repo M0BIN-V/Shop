e95ae257f95648a220d51e58e7855c4378a563f9
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-09-22 15:54:08 +0330

    fix: allow any header in presentation

--- Src/Peresentation/Api/ServiceInstallers/PeresentationServicesInstaller.cs
+++ Src/Peresentation/Api/ServiceInstallers/PeresentationServicesInstaller.cs
@@ -5,7 +5,12 @@ public class PeresentationServicesInstaller : IServiceInstaller
     public void Install(IServiceCollection services, IConfiguration configuration)
     {
         services
-            .AddCors(options => options.AddPolicy(name: "AllowAnyOrigin", policy => policy.AllowAnyOrigin()))
+            .AddCors(options => options.AddPolicy(name: "AllowAnyOrigin", policy =>
+            {
+                policy.AllowAnyOrigin();
+                policy.AllowAnyHeader();
+                policy.AllowAnyMethod();
+            }))
 
             .AddSwaggerGen()
             .AddEndpointsApiExplorer()
