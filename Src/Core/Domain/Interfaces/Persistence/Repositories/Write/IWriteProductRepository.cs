ca5920307fda287c492e3d7a7f7f636e813a35ae
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-09-25 13:28:13 +0330

    feat: add WriteProductRepository

---
+++ Src/Core/Domain/Interfaces/Persistence/Repositories/Write/IWriteProductRepository.cs
@@ -0,0 +1,7 @@
+﻿using Domain.Interfaces.Persistence.Repositories.Write.Abstractions;
+
+namespace Domain.Interfaces.Persistence.Repositories.Write;
+
+public interface IWriteProductRepository : IWriteEntityRepositoryBase<Product>
+{
+}
