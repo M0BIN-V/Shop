01cf10c17aba3c90f2e7f2626e45f25b6d1c2c6c
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-09-25 13:28:31 +0330

    feat: add ReadProductRepository

---
+++ Src/Core/Domain/Interfaces/Persistence/Repositories/Read/IReadProductsRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Interfaces.Persistence.Repositories.Read.Abstractions;
+
+namespace Domain.Interfaces.Persistence.Repositories.Read;
+
+public interface IReadProductsRepository : ICodedEntityRepository<Product>
+{
+    public Task<bool> ExistsAsync(Name Name);
+
+}
