ef5aadb61f602dda8831aeb1b4821bddb49ddd4d
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-09-30 11:15:24 +0330

    refactore: refactore usings

--- Src/Peresentation/Api/GlobalUsings.cs
+++ Src/Peresentation/Api/GlobalUsings.cs
@@ -1,9 +1,10 @@
-﻿global using Domain.Errors;
+﻿global using Application;
+global using DiServiceInstaller;
+global using Domain.Errors;
 global using Domain.ValueObjects;
 global using MediatR;
-global using static Microsoft.AspNetCore.Http.StatusCodes;
-global using DiServiceInstaller;
+global using Microsoft.AspNetCore.Mvc;
 global using Persistence;
-global using Application;
 global using Resulver.AspNetCore.WebApi;
 global using Resulver.AspNetCore.WebApi.Response;
+global using static Microsoft.AspNetCore.Http.StatusCodes;
