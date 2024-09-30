f729563c36e054f9f06f314e69f795abb2458054
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-09-30 19:43:39 +0330

    feat: add ProductErrorsProfile

---
+++ Src/Peresentation/Api/ErrorProfiles/ProductErrorsProfile.cs
@@ -0,0 +1,11 @@
+﻿using Resulver.AspNetCore.WebApi.ErrorHandling.Abstraction;
+
+namespace Api.ErrorProfiles;
+
+public class ProductErrorsProfile : ErrorProfile
+{
+    public ProductErrorsProfile()
+    {
+        AddError<ProductAlreadyExistsError>().WithStatusCode(Status409Conflict);
+    }
+}
